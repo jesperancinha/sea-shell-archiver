a201824da1454ac1b72e159095e8c3f10a0af807
Joao Esperancinha MS-WS <jofisaes@gmail.com>
2021-08-03 18:59:56 +0200

    Windows tests

---
+++ docs/windows/WindowsNotes.md
@@ -0,0 +1,10 @@
+# Sea Shell Archiver Windows Installation and Setup notes
+
+1. https://chocolatey.org/install
+   - Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+   - run [setup.ps1](./setup.ps1)
+   - Set-ExecutionPolicy -Scope Process -ExecutionPolicy Default
+2. https://plugins.jetbrains.com/plugin/10249-powershell/features
+3. https://github.com/shyiko/jabba
+4. https://www.azul.com/blog/eclipse-temurin-new-distribution-of-openjdk/
+5. echo $env:JAVA_HOME
---
+++ docs/windows/setup.ps1
@@ -0,0 +1,9 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install maven -y
+choco uninstall maven -y
+choco install maven -y
+choco install git -y
+
+curl https://github.com/adoptium/temurin16-binaries/releases/download/jdk-16.0.2%2B7/OpenJDK16U-jdk_x64_windows_hotspot_16.0.2_7.msi -o jdk16.msi
+jdk16.msi
