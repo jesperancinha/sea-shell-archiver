460b35cf43890693dfe7ceecba07c5dc3a90ac0a
Joao Esperancinha <jofisaes@gmail.com>
2021-08-05 09:11:16 +0200

    🌿💿 Proving non-blocking

---
+++ sea-shell-service-immutable/src/test/java/org/jesperancinha/shell/webflux/immutable/service/SeaShellsReactiveImmutableServiceTest.java
@@ -0,0 +1,50 @@
+package org.jesperancinha.shell.webflux.immutable.service;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.jesperancinha.shell.SeaShellWiremockSoapLauncher;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.blockhound.BlockHound;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static reactor.core.publisher.Mono.delay;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class SeaShellsReactiveImmutableServiceTest {
+
+    private static WireMockServer wireMockServer;
+
+    @Autowired
+    private SeaShellsReactiveImmutableService seaShellService;
+
+    @BeforeAll
+    public static void setUpAll() throws IOException {
+        BlockHound.install();
+        wireMockServer = SeaShellWiremockSoapLauncher.createWireMockServer();
+    }
+
+    @Test
+    public void findAllCompleteSeaShells_onCall_thenNonBlocking() {
+        assertAll(() -> delay(Duration.ofSeconds(1))
+                .doOnNext(it -> seaShellService.getAllShells())
+                .block());
+    }
+    @Test
+    public void findAllCompleteSeaShellById1s_onCall_thenNonBlocking() {
+        assertAll(() -> delay(Duration.ofSeconds(1))
+                .doOnNext(it -> seaShellService.getSeaShellById(1L))
+                .block());
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        wireMockServer.stop();
+    }
+}
