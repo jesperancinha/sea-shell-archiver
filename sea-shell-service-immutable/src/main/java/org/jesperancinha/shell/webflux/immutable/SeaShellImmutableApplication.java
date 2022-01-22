4e3d55b7147f60e8f46f7f58c6bf6611db0e89b1
Joao Esperancinha <jofisaes@gmail.com>
2022-01-22 09:54:09 +0100

    🌿 Build Fix

--- sea-shell-service-immutable/src/main/java/org/jesperancinha/shell/webflux/immutable/SeaShellImmutableApplication.java
+++ sea-shell-service-immutable/src/main/java/org/jesperancinha/shell/webflux/immutable/SeaShellImmutableApplication.java
@@ -3,8 +3,9 @@ package org.jesperancinha.shell.webflux.immutable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = WebMvcAutoConfiguration.class)
+@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class, ErrorMvcAutoConfiguration.class})
 public class SeaShellImmutableApplication {
     public static void main(String[] args) {
         SpringApplication.run(SeaShellImmutableApplication.class, args);
--- sea-shell-service-spring-web-flux/sea-shell-rest-service/src/main/java/org/jesperancinha/shell/SeaShellApplication.java
+++ sea-shell-service-spring-web-flux/sea-shell-rest-service/src/main/java/org/jesperancinha/shell/SeaShellApplication.java
@@ -3,10 +3,11 @@ package org.jesperancinha.shell;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableWebFlux
-@SpringBootApplication(exclude = WebMvcAutoConfiguration.class)
+@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class, ErrorMvcAutoConfiguration.class})
 public class SeaShellApplication {
 
     public static void main(String[] args) {
